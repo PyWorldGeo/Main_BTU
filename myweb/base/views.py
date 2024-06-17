50c4de6f1b48a7e9783072640c01ef1151de8aaa
12DoRoRo12 <n.tsitsqishvili@gmail.com>
2024-06-17 15:37:13 +0400

    cards

---
+++ myweb/base/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+
+
+# Create your views here.
+def home(request):
+    books = Book.objects.all()
+    context = {"books": books}
+    return render(request, 'base/home.html', context)
+
+
+def about(request):
+    return render(request, 'base/about.html')
